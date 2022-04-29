bad1a05ad7416d434b4995cb112928d499b8b120
Daniel Stoyanov <daniel.radst@gmail.com>
2022-04-29 17:54:19 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var spacing_x = 40;
+var spacing_y = 100;
+// Initialize Flowy - Do Campaing Flow magic here
+flowy(document.getElementById("canvas"), onGrab, onRelease, onSnap, onRearrange, spacing_x, spacing_y);
+function onGrab(block){
+	// When the user grabs a block
+}
+function onRelease(){
+	// When the user releases a block
+}
+function onSnap(block, first, parent){
+	// When a block snaps with another one
+}
+function onRearrange(block, parent){
+	// When a block is rearranged
+}
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'body': '#F8F8F8',
+        'dark':'#030234',
+        'container': '#FFFFFF',
+        'editor':'#EBEBEB',
+        'e-c-border':'#0000001F',
+        'navi':'#050038',
+        'darknavi':'#030234',
+        'blue':'#4262FF',
+        'gray':'#E6E6E6',
+        'darkgray':'#808080',
+        'separator':'#EBEBF2',
+        'e-split':'#D9E0FF',
+        'e-delay':'#EBEBEB',
+        'e-abtest':'#CDCCD7',
+        'e-s-border':'#000029',
+        'e-s-textcolor':'#808080',
+        'e-s-lines':'#BFBFBF',
+      },
+      fontFamily:{
+        'opensans':["'Open Sans'",'sans-serif'],
+        'roboto':["'Roboto'",'sans-serif'],
+        'formular':["'Formular'",'sans-serif'],
+        'arial':["'Arial'",'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
